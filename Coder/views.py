d7bedfc9509a66d1476eb046a8f65dafe3b44d35
FabricioOjeda <fafaojedasemidey@gmail.com>
2022-09-05 17:34:03 +0200

    Ejercicio clase 18

--- Coder/views.py
+++ Coder/views.py
@@ -10,9 +10,9 @@ def familiares(request):
     familiar1 = Familiar(nombre="Gabriela",apellido="Ojeda",parentesco="hermana", edad = 33 , nacimiento = "2022-1-1" , email="email@etc")
     familiar2 = Familiar(nombre="Leonardo",apellido="Ojeda",parentesco="hermano", edad = 23 , nacimiento = "2022-1-1" , email="email2@etc")
     familiar3 = Familiar(nombre="Maria",apellido="Corsi",parentesco="abuela", edad = 90 , nacimiento = "2022-1-1" , email="404 not found lol")
-    #familiar1.save()
-    #familiar2.save()
-    #familiar3.save()
+    familiar1.save()
+    familiar2.save()
+    familiar3.save()
     texto1= f"Nombre{familiar1.nombre},apellido{familiar1.apellido},parentesco{familiar1.parentesco},edad{familiar1.edad},fecha de nacimiento{familiar1.nacimiento},email{familiar1.email}"
     texto2= f"Nombre{familiar2.nombre},apellido{familiar2.apellido},parentesco{familiar2.parentesco},edad{familiar2.edad},fecha de nacimiento{familiar2.nacimiento},email{familiar2.email}"
     texto3= f"Nombre{familiar3.nombre},apellido{familiar3.apellido},parentesco{familiar3.parentesco},edad{familiar3.edad},fecha de nacimiento{familiar3.nacimiento},email{familiar3.email}"
